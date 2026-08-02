9abd792dfa50a37334a107c4fcfc3c108d0cdb99
Alexandru <alexandru.nic14@gmail.com>
2026-08-02 17:25:52 +0100

    Project deployed

--- Server/utils/Email.js
+++ Server/utils/Email.js
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendResetPasswordLink(email, token) {
     const FRONTEND_URL = `${process.env.FRONTEND_URL}/reset-password?token=${token}`;
 
-    try { 
+    try {
         await resend.emails.send({
             from: "Password Reset Submission <oboarding@resend.dev>",
             to: email,
